c383ed7d30dfc1a7df50a2741cac5ff8c58ecfad
soddokayo <soddokayo@gmail.com>
2023-06-19 13:48:14 +0900

    init react, 파일 업로드 및 FileReader로 console.log 텍스트출력

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import FileUploader from './FileUploader';
 
-function App() {
+const App = () => {
+  const [fname, setFname] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FileUploader />
     </div>
   );
 }
---
+++ src/FileUploader.js
@@ -0,0 +1,48 @@
+import { React, useEffect, Fragment, useRef } from "react";
+import * as bootstrap from "bootstrap";
+
+const FileUploader = () => {
+  const fileInput = useRef(null);
+  const handleButtonClick = (e) => {
+    fileInput.current.click();
+  };
+  const handleChange = (e) => {
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      const dump = fileReader.result;
+      //console.log(dump);
+      const lines = dump.split(/\r?\n/);
+      console.log(lines);
+    };
+    fileReader.readAsText(file);
+  };
+
+  useEffect(() => {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+  })
+  
+  return (
+      <>
+      <div class="card text-center mb-3" style={{width: '18 rem'}}>
+      <div class="card-body">
+        <h5 class="card-title font-weight-bold">Chat Tagger (디지털포렌식 조사 지원)</h5>
+        <p class="card-text">카카오톡 또는 텔레그램 대화 내역을 txt파일로 업로드해보세요.</p>
+        <Fragment>
+          <button onClick={handleButtonClick} class="btn btn-primary" data-bs-toggle="popover" data-bs-title="파일 업로드" data-bs-content="카카오톡 또는 텔레그램 대화 내역을 txt파일로 업로드해보세요.">파일 업로드</button>
+          <input type="file"
+            ref={fileInput}
+            onChange={handleChange}
+            style={{ display: "none" }}
+          />
+        </Fragment>
+        <br /><br /><br /><button type="button" class="btn btn-danger" data-bs-toggle="popover" data-bs-title="Popover title" data-bs-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+      </div>
+    </div>
+      </>
+  )
+};
+
+export default FileUploader;
